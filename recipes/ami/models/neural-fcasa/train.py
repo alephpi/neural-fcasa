ea5cc53087fab59ef9716897a36c2ecf0af42840
alephpi <maosicheng98@gmail.com>
2025-05-16 15:23:35 +0200

    remove dependency of aiaccel.apps.train and mpi config

---
+++ recipes/ami/models/neural-fcasa/train.py
@@ -0,0 +1,52 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+from hydra.utils import instantiate
+from omegaconf import OmegaConf as oc  # noqa: N813
+
+import lightning as L
+
+
+# import debugpy
+# try:
+#     debugpy.listen(("localhost", 9501))
+#     print("Waiting for debugger attach")
+#     debugpy.wait_for_client()
+# except Exception as e:
+#     pass
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument("config", type=Path, help="Config file in YAML format")
+    parser.add_argument("--working_directory", type=Path, default=Path.cwd(), help="Working directory")
+    args, unk_args = parser.parse_known_args()
+
+    # load config
+    config = oc.merge(
+        {
+            "base_config_path": str(Path(__file__).parent / "config"),
+            "base_config": "${base_config_path}/train_base.yaml",
+        },
+        vars(args),
+        oc.load(args.config),
+        oc.from_cli(unk_args),
+    )
+
+    config = oc.merge(
+        oc.load(config.base_config),
+        config,
+    )
+
+    # train
+    trainer: L.Trainer = instantiate(config.trainer)
+    model = instantiate(config.task)
+    datamodule = instantiate(config.datamodule)
+    trainer.fit(
+        model=model,
+        datamodule=datamodule,
+    )
+
+
+if __name__ == "__main__":
+    main()
